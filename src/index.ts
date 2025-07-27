1d1c199d177766ea59db96ee8a50d7a4985c2668
AlexRiosXI <alexriosxi@gmail.com>
2025-07-27 12:58:36 -0600

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { default as useRequest } from './hooks/useRequest';
+import { default as useMutation } from './hooks/useMutation';
+import { Request, MutationRequest } from './types';
+import { generateInitialState } from './utils/stateGenerators';
+
+export { useRequest, useMutation, generateInitialState }
+
+export type { Request, MutationRequest }
